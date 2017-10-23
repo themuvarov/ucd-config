ed160b7178e6a8012c2ca3b4069ab89c1133b78f
Rafal Sontowski <rafal.sontowski@redknee.com>
2017-10-23 12:12:21 +0200

    added project specific config files

---
+++ ocs/k8s/INTP/scripts/proc/project_init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This is placeholder for custom init script
+# Include custom init logic here if necessary
+
+
+#THIS_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#echo This script dir: $THIS_SCRIPT_DIR
+#echo Source: ${BASH_SOURCE[0]}
+#echo Working dir: $(pwd)
+#echo params: $@
